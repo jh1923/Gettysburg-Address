ab2de96cdab090dd7c91b5ea0f5e25c0f3baa58a
jh1923 <42978576+jh1923@users.noreply.github.com>
2018-11-09 00:28:27 -0500

    Update AddressMath.java

--- AddressMath.java
+++ AddressMath.java
@@ -10,6 +10,7 @@ public class AddressMath {
 		//creates scanner object and new array list
 		Scanner file = new Scanner(new File("GettysburgAddress"));
 		ArrayList<String> addressWords = new ArrayList<String>();
+		
 		//adds all words in speech to array list
 		while (file.hasNext())
 		{
@@ -17,21 +18,53 @@ public class AddressMath {
 			addressWords.add(word);
 		}
 		
-		String word1 = addressWords.get(0);
-		
+		//for each word in array list
 		for (int i=0; i < addressWords.size(); i++)
 		{
-			String word2 = addressWords.get(i);
+			String currentWord = addressWords.get(i);
 			
-			//compares each following word, eventually setting word1 equal to the longest word
-			if (word2.length()>word1.length())
+			//removes punctuation from each word in list
+			for (int n=0; n < currentWord.length(); n++)
 			{
-				word1 = word2;
+				//accounts for spaces, commas, and periods
+				if (currentWord.charAt(n)==' '|| currentWord.charAt(n)==',' || currentWord.charAt(n)=='.')
+				{
+					addressWords.set(i, currentWord.substring(0, n));
+				}
+				
+				//accounts for hyphens
+				if (currentWord.charAt(n)=='-' && currentWord.charAt(n+1)!='-')
+				{
+					addressWords.set(i, currentWord.substring(0, n));
+					addressWords.add(i+1, currentWord.substring(n+1));
+				}
+				
+				//accounts for em dashes i.e. double hyphens
+				else if (currentWord.charAt(n)=='-' && currentWord.charAt(n+1)=='-')
+				{
+					addressWords.set(i, currentWord.substring(0, n));
+					addressWords.add(i+1, currentWord.substring(n+2));
+				}
 			}
-			
 		}
 		
-		System.out.print("longest: " + word1);
+		//compares each element in array list to find and print longest word
+		String longestWord = addressWords.get(0);
+		for (int i=0; i < addressWords.size(); i++)
+		{
+			String currentWord = addressWords.get(i);
+			if (currentWord.length() > longestWord.length())
+				longestWord = currentWord;
+		}
+		System.out.println("The longest word is '" + longestWord + "' with a length of " + longestWord.length());
 		
+		//finds and prints average word length by dividing total characters over number of words
+		double totalCharacters = 0;
+		for (int i=0; i < addressWords.size(); i++)
+		{
+			//finds total number of characters by adding length of each word in array list
+			totalCharacters += (addressWords.get(i)).length();
+		}
+		System.out.println("Average word length: " + totalCharacters/addressWords.size() );
 	}
 }
